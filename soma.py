401aa2d9c605c8921b465df2f829953b8d370c7f
marcelomsf <marcelo.messiasfranco@gmail.com>
2020-05-26 20:49:08 -0300

    alteracao em soma para so aceitar números

--- soma.py
+++ soma.py
@@ -1,3 +1,3 @@
-x = input("Digite o primeiro número: ")
-y = input("Digite o segundo número: ")
-print ("O resultado da soma é : "x + y)
+x = int(input("Digite o primeiro número: "))
+y = int(input("Digite o segundo número: "))
+print ("O resultado da soma é : ",x + y)
