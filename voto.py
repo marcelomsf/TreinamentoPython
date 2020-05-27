75a9dc53743f4370134b4fef539fc190e54d3faf
marcelomsf <marcelo.messiasfranco@gmail.com>
2020-05-26 23:27:35 -0300

    adicionando o else ao if

--- voto.py
+++ voto.py
@@ -2,6 +2,10 @@
 # idade
 # Se idade < 16 então :
 # - Você não pode votar
-idade = int( input ("Quantos anos você tem ? ") )
+idade = int( input ("Quantos anos você tem ?\n") )
 if ( idade < 16 ):
     print( "Você não pode votar!" )
+else:
+    print( "Você pode votar!")
+
+print ("FIM")    
