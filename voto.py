5d62ea8ff022a88af7b1995853d416771ad25c59
marcelomsf <marcelo.messiasfranco@gmail.com>
2020-05-26 23:50:18 -0300

    refatorando o codigo para eliminar um if

--- voto.py
+++ voto.py
@@ -5,11 +5,10 @@
 idade = int( input ("Quantos anos você tem ?\n") )
 if ( idade < 16 ):
     print("Você não pode votar!" )
-elif ( idade < 18 ):
+elif ( idade < 18  or idade > 70 ):
     print("Você pode votar!")
 elif ( idade < 70 ):
     print("Você deve votar")
-else:
-    print("Você pode votar")
+
 
 print ("FIM")    
