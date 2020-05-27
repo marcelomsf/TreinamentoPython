fd146c4b22c5ae828c645d8129f89add9e36c4e1
marcelomsf <marcelo.messiasfranco@gmail.com>
2020-05-27 18:33:49 -0300

    Alterando a versão para sempre trocar letras por letras

--- crypto.py
+++ crypto.py
@@ -10,14 +10,26 @@
 
 alfabeto = ['a', 'b', 'c', 'd' , 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm'
             'n', 'o', 'p', 'q', 'r', 's', 't' ,'u', 'v','w', 'x', 'y', 'z']
-tamanho_alfabeto = len(alfabeto)
+#tamanho_alfabeto = len(alfabeto)
+tamanho_alfabeto = 128
+limite_alfabeto_minisculo=122
+limite_alfabeto_maisculo= 90
+letras_alfabeto = 26
+
 chave = 3
-mensagem = "abacaxi"
+mensagem = "abcdefghijklmnopqrstuvwxyz"
+mensagem = mensagem.upper()
 cifrada = ""
 for letra in mensagem:
-    print(letra)
-    indice = alfabeto.index(letra)
-    nova_letra = alfabeto[(indice + chave)%tamanho_alfabeto]
+    # print(letra)
+    # indice = alfabeto.index(letra)
+    indice = ord(letra)
+    #nova_letra = alfabeto[(indice + chave)%tamanho_alfabeto]
+    novo_indice = (indice + chave)%tamanho_alfabeto
+    if ( novo_indice > limite_alfabeto_minisculo or ( novo_indice > limite_alfabeto_maisculo and novo_indice < 97 )):
+        novo_indice = novo_indice - letras_alfabeto
+
+    nova_letra = chr( novo_indice )
     cifrada = cifrada + nova_letra
 
 print(mensagem)
