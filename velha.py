5a48b3b37b4b4fa420ee5371915ef9b6affc4de9
marcelomsf <marcelo.messiasfranco@gmail.com>
2020-05-27 16:09:31 -0300

    Primeira versao finalizada

--- velha.py
+++ velha.py
@@ -8,21 +8,51 @@ tabuleiro = []
 #               3, 4, 5
 #               6, 7, 8]
 
+tabuleiro = [X, O, X,
+            O, X, O,
+            O, VAZIO,X]
+
+
+tabuleiro = [VAZIO, VAZIO, VAZIO,
+            VAZIO, VAZIO, VAZIO,
+            VAZIO, VAZIO,VAZIO]
+
+
+tabuleiro = [X, O, X,
+            X, X, VAZIO,
+            O, O, O]
+
+tabuleiro = [X, O, X,
+            O, X, O,
+            O, X, O]
+
+alinhamento = False
+vencedor = VAZIO
+
 # Horizontal
 for i in range(0, 9, 3):
     if tabuleiro[i] == tabuleiro[i+1] == tabuleiro [i+2] != VAZIO:
-        print("O jogo acabou")
+        alinhamento = True
+        vencedor = tabuleiro[i]
 
 # Vertical
-for i in range(0,3):
-    if tabuleiro[i] == tabuleiro[i+3] == tabuleiro [i+6] != VAZIO:
-        print("O jogo acabaou")
+if not alinhamento:    
+    for i in range(0,3):
+        if tabuleiro[i] == tabuleiro[i+3] == tabuleiro [i+6] != VAZIO:
+            alinhamento = True
+            vencedor = tabuleiro[i]
 
 # Diagonal
-for i in range(0, 3, 2):
-    if tabuleiro[0+i] == tabuleiro[4] == tabuleiro [8-i] != VAZIO:
-        print("O jogo acabaou")
+if not alinhamento:
+    for i in range(0, 3, 2):
+        if tabuleiro[0+i] == tabuleiro[4] == tabuleiro [8-i] != VAZIO:
+            alinhamento = True
+            vencedor = tabuleiro[i]
 
-# Empate
-if not VAZIO in tabuleiro:
-    print("Jogo empatou: Deu velha")
+
+# Final do Jogo
+if alinhamento:
+    print("Vencedor: ",vencedor)
+else:
+    if not VAZIO in tabuleiro:
+        print("Jogo empatou: Deu velha")
