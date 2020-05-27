6f994ddf7f663f2ec6196d8ed22ddccb387ebcbe
marcelomsf <marcelo.messiasfranco@gmail.com>
2020-05-27 14:50:11 -0300

    Programa de exemplo para criação de lista

---
+++ aprovados.py
@@ -0,0 +1,8 @@
+aprovados = [
+    "Graziela Barroso", "Juliano Moreira", "André Rebouças", "César Lattes","Enedina Alves",
+    "Machado de Assis", "Ayrton Senna", "Luiz Gamma", "Ruth de Souza", "Nise da Silveira",
+    "Johanna Döbereiner", "Carolina de Jesus", "Leopoldo Nachbin", "Antonieta de Barros", "Lima Barreto"
+    ]
+
+print("A lista: ", aprovados)
+print ("A primeira colocado: ", aprovados[0])
