eb7293f6a82512b1dda05f7fa2a60e3c8840a7a7
marcelomsf <marcelo.messiasfranco@gmail.com>
2020-05-26 21:34:02 -0300

    primeiro exemplo de execucao do if

---
+++ voto.py
@@ -0,0 +1,7 @@
+# Quantos anos você tem?
+# idade
+# Se idade < 16 então :
+# - Você não pode votar
+idade = int( input ("Quantos anos você tem ? ") )
+if ( idade < 16 ):
+    print( "Você não pode votar!" )
