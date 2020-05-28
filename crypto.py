fbea6e2a174145f066fdcc3841d16d53ac71cef6
marcelomsf <marcelo.messiasfranco@gmail.com>
2020-05-27 23:32:12 -0300

    Alterando a versão para sempre trocar letras por letras 0.1

--- crypto.py
+++ crypto.py
@@ -12,12 +12,15 @@ alfabeto = ['a', 'b', 'c', 'd' , 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm'
             'n', 'o', 'p', 'q', 'r', 's', 't' ,'u', 'v','w', 'x', 'y', 'z']
 #tamanho_alfabeto = len(alfabeto)
 tamanho_alfabeto = 128
-limite_alfabeto_minisculo=122
-limite_alfabeto_maisculo= 90
-letras_alfabeto = 26
+#Limetes em relacao a tabela ASCII
+limite_supeior_minisculo=ord('z')
+limite_inferior_minisculo=ord('a')
+limite_superior_maiusculo= ord('Z')
+limite_inferior_maiusculo = ord('A')
+letras_alfabeto = limite_supeior_minisculo - limite_inferior_minisculo + 1
 
 chave = 3
-mensagem = "abcdefghijklmnopqrstuvwxyz"
+mensagem = "AbcdefghijklmnopqrstuvwxyZ"
 mensagem = mensagem.upper()
 cifrada = ""
 for letra in mensagem:
@@ -26,8 +29,9 @@ for letra in mensagem:
     indice = ord(letra)
     #nova_letra = alfabeto[(indice + chave)%tamanho_alfabeto]
     novo_indice = (indice + chave)%tamanho_alfabeto
-    if ( novo_indice > limite_alfabeto_minisculo or ( novo_indice > limite_alfabeto_maisculo and novo_indice < 97 )):
+    if (( novo_indice > limite_supeior_minisculo )or ( novo_indice > limite_superior_maiusculo and novo_indice < limite_inferior_minisculo)):
         novo_indice = novo_indice - letras_alfabeto
+    
 
     nova_letra = chr( novo_indice )
     cifrada = cifrada + nova_letra
